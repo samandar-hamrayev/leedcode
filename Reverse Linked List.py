ce2ce4ca2587a6ac93a15cc360e369ee5e248180
hamrayev7 <samandarhamrayev555@gmail.com>
2025-02-09 19:50:53 +0500

    reverse linked listda reverse qilish logikasi ochib ketgan ekan shuni sozlab qoydim

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -6,7 +6,16 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr =  head
+        while curr:
+            next_node = curr.next
 
+            curr.next = prev
+
+            prev = curr
+            curr = next_node
+        return prev
 
 
 def create_linked_list(values):
