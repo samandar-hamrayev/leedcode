c6f6d2bb02273522ee421dca55b9e9f6ba9b6274
hamrayev7 <samandarhamrayev555@gmail.com>
2025-03-23 14:09:57 +0500

    new for leetcode

---
+++ Combination Sum IV.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combinationSum4(self, nums: list[int], target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for i in range(1, target + 1):
+            for num in nums:
+                if i >= num:
+                    dp[i] += dp[i - num]
+        return dp[target]
+
+sol = Solution()
+print(sol.combinationSum4(nums = [1,2,3], target = 4))
+
