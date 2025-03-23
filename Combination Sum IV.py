fc15eb8719afb666eca4a294adac8268bd1bc28b
hamrayev7 <samandarhamrayev555@gmail.com>
2025-03-23 15:43:59 +0500

    upd

--- Combination Sum IV.py
+++ Combination Sum IV.py
@@ -2,7 +2,6 @@ class Solution:
     def combinationSum4(self, nums: list[int], target: int) -> int:
         dp = [0] * (target + 1)
         dp[0] = 1
-
         for i in range(1, target + 1):
             for num in nums:
                 if i >= num:
@@ -11,4 +10,3 @@ class Solution:
 
 sol = Solution()
 print(sol.combinationSum4(nums = [1,2,3], target = 4))
-
